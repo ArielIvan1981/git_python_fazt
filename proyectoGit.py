b0b6386d18c00d464bb9bc5bdc00ead20999dbe2
Ivan <arielivan_herrera@hotmail.com>
2022-05-28 09:43:02 -0300

    agregue un bucle

--- proyectoGit.py
+++ proyectoGit.py
@@ -1,2 +1,6 @@
 # Ejemplo para estudiar Git
 print ("Antes del commit")
+# bucle agregado despues del primer commit
+print ("Bucle agregado despues del primer commit")
+for i in range(5):
+    print (i)
